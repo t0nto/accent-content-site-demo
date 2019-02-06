9142a37959b2e551481b8715fcc3a2c8171667a5
Tom Wells <tom@Toms-MacBook-Air.local>
2019-02-06 16:22:17 +0100

    exclude index markdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ exports.createPages = ({ actions, graphql }) => {
 
   return graphql(`
     {
-      allMarkdownRemark(limit: 1000) {
+      allMarkdownRemark(limit: 1000, , filter: {frontmatter: {path: {ne: "/profile"}}}) {
         edges {
           node {
             id
